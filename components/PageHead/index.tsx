c9b1debf516785f1fb1b42fbb0d01f854443f9fa
derek-perry <dp@dp.design>
2023-04-09 20:22:37 -0400

    Fix Head and Footer on 404/500
    The PageHead and PageFooter components were not added to the 404 and 500 error pages in the last commit. Thus it hath been fixeth.

--- components/PageHead/index.tsx
+++ components/PageHead/index.tsx
@@ -31,7 +31,7 @@ const PageHead: FC<IPageHeadProps> = ({
       <meta property="og:type" content="website" />
       <meta property="og:title" key="title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:url" content={process.env.NEXT_PUBLIC_SITE_URL + '/' + url || 'https://cosmoscleaner.com/' + url || 'https://cosmoscleaner.com'} />
+      <meta property="og:url" content={process.env.NEXT_PUBLIC_SITE_URL + '/' + url || 'https://cosmoscleaner.com'} />
       <meta property="og:site_name" content="Cosmos Cleaner" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@_derekperry" />
@@ -41,7 +41,7 @@ const PageHead: FC<IPageHeadProps> = ({
       <meta name="twitter:image:width" content="1920" />
       <meta name="twitter:image:height" content="1080" />
       <meta name="twitter:image:alt" content="Banner for CosmosCleaner.com with centered black text on white background" />
-      <link rel="canonical" href={process.env.NEXT_PUBLIC_SITE_URL || '/'} />
+      <link rel="canonical" href={process.env.NEXT_PUBLIC_SITE_URL || 'https://cosmoscleaner.com'} />
 
       <meta name="theme-color" content="#000000" />
       <link rel="icon" href={process.env.NEXT_PUBLIC_SITE_URL + '/favicon.ico' || 'https://cosmoscleaner.com/favicon.ico'} />
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,16 +1,12 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import PageHead from "../components/PageHead"
+import PageFooter from "../components/PageFooter"
 import LinkInternal from "../components/Links/LinkInternal"
-import LinkExternal from "../components/Links/LinkExternal"
 
 const error500Page: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Error 500: Internal Server Error</title>
-        <link rel="icon" href={process.env.NEXT_PUBLIC_SITE_URL + '/favicon.ico' || './favicon.ico'} />
-      </Head>
+      <PageHead title="Error 500: Internal Server Error" description="Cosmos Cleaner is a 2.5D space vehicle arcade collect-a-thon game." url="500" />
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <div className="flex w-full flex-row items-center justify-center text-center">
@@ -26,14 +22,7 @@ const error500Page: NextPage = () => {
         </p>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <div
-          className="flex items-center justify-center gap-2"
-        >
-          <LinkExternal href="https://github.com/derek-perry/CosmosCleaner" title="Cosmos Cleaner Game GitHub" className="no-underline"><p>Cosmos Cleaner</p></LinkExternal> - <LinkExternal href="https://dp.design" title="Derek Perry" className="no-underline"><p>Derek Perry</p></LinkExternal>{' '}
-          <Image src={process.env.NEXT_PUBLIC_SITE_URL + '/dp.svg' || './dp.svg'} alt="Derek Perry dp.design Logo" width={50} height={50} />
-        </div>
-      </footer>
+      <PageFooter />
     </div>
   )
 }
