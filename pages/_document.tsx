cce6582c8b2f8882ec79ebb568328b721a8f9d07
DP <dp@dp.design>
2023-09-06 14:43:26 -0400

    Improve robots.txt Generation
    The robots.txt now disallows robots from access any irrelevant nextjs files. (as per reddit.com/r/nextjs/comments/u0soy3/update_your_robotstxt_to_help_google_index_your)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,55 +6,55 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <Script type="application/ld+json" strategy="beforeInteractive">{`
-          "@context":"https://schema.org",
-          "@type":"SoftwareApplication",
-          "name":"Cosmos Cleaner",
-          "description":"Clean up space junk, upgrade your ship, and become the best cleaner in the cosmos!",
-          "abstract":"Clean up space junk, upgrade your ship, and become the best cleaner in the cosmos!",
-          "gameLocation":"Space",
-          "applicationCategory":"GameApplication",
-          "applicationSubCategory":"Arcade Game",
-          "genre":"arcade",
-          "playMode":"Single-Player",
-          "keywords":"[cosmos,cleaner,game,space]",
-          "operatingSystem":"[Windows 10,Windows 11]",
-          "gamePlatform":"[Windows 10,Windows 11]",
-          "url":"https://cosmoscleaner.com",
-          "downloadUrl":"https://cosmoscleaner.com/play",
-          "installUrl":"https://cosmoscleaner.com/play",
-          "thumbnailUrl":"https://cosmoscleaner.com/banner.png",
-          "image":"https://cosmoscleaner.com/banner.png",
-          "screenshot":"https://cosmoscleaner.com/banner.png",
+          "@context": "https://schema.org/",
+          "@type": "SoftwareApplication",
+          "name": "Cosmos Cleaner",
+          "description": "Clean up space junk, upgrade your ship, and become the best cleaner in the cosmos!",
+          "abstract": "Clean up space junk, upgrade your ship, and become the best cleaner in the cosmos!",
+          "gameLocation": "Space",
+          "applicationCategory": "GameApplication",
+          "applicationSubCategory": "Arcade Game",
+          "genre": "arcade",
+          "playMode": "Single-Player",
+          "keywords": "[cosmos,cleaner,game,space]",
+          "operatingSystem": "[Windows 10,Windows 11]",
+          "gamePlatform": "[Windows 10,Windows 11]",
+          "url": "https://cosmoscleaner.com",
+          "downloadUrl": "https://cosmoscleaner.com/play",
+          "installUrl": "https://cosmoscleaner.com/play",
+          "thumbnailUrl": "https://cosmoscleaner.com/banner.png",
+          "image": "https://cosmoscleaner.com/banner.png",
+          "screenshot": "https://cosmoscleaner.com/banner.png",
           "aggregateRating": {
             "@type": "AggregateRating",
             "ratingValue": "0",
             "ratingCount": "0"
           },
           "offers": {
-            "@type":"Offer",
-            "name":"Cosmos Cleaner",
-            "description":"Free",
+            "@type": "Offer",
+            "name": "Cosmos Cleaner",
+            "description": "Free",
             "price": 0,
-            "priceCurrency":"USD"
+            "priceCurrency": "USD"
           },
           "isAccessibleForFree": true,
           "author": {
-            "@type":"Person",
-            "name":"Derek Perry",
-            "url":"https://dp.design",
-            "email":"dp@dp.design"
+            "@type": "Person",
+            "name": "Derek Perry",
+            "url": "https://dp.design",
+            "email": "dp@dp.design"
           },
-          "creditText":"Game and Website by Derek Perry",
-          "processorRequirements":"Quad-core Intel or AMD 2.5 GHz or faster",
-          "storageRequirements":"2GB",
-          "memoryRequirements":"4GB",
-          "copyrightNotice":"Cosmos Cleaner Copyright 2023 Derek Perry",
-          "copyrightYear":"2023",
-          "datePublished":"2023-04-09",
-          "dateCreated":"2023-04-09",
-          "dateModified":"2023-09-06T19:18",
+          "creditText": "Game and Website by Derek Perry",
+          "processorRequirements": "Quad-core Intel or AMD 2.5 GHz or faster",
+          "storageRequirements": "2GB",
+          "memoryRequirements": "4GB",
+          "copyrightNotice": "Cosmos Cleaner Copyright 2023 Derek Perry",
+          "copyrightYear": "2023",
+          "datePublished": "2023-04-09",
+          "dateCreated": "2023-04-09",
+          "dateModified": "2023-09-06T19:30",
           "isFamilyFriendly": true,
-          "interactivityType":"active",
+          "interactivityType": "active",
           "accessibilityControl": [
             "fullKeyboardControl",
             "fullMouseControl",
@@ -65,7 +65,7 @@ export default function Document() {
             "motionSimulation",
             "sound"
           ],
-          "inLanguage":"en-US"
+          "inLanguage": "en-US"
         `}</Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
