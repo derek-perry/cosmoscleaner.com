63fa8f626f159191e9bcd062a3895352106fade6
DP <dp@dp.design>
2023-09-06 20:59:22 -0400

    Fix ld+json dateModified
    The date/time modified for the ld+json script was not modified in the last commit. So now it has been. (It was also fixed to be September 7th instead of September 6th.)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -64,7 +64,7 @@ export default function Document() {
         "copyrightYear": "2023",
         "datePublished": "2023-04-09",
         "dateCreated": "2023-04-09",
-        "dateModified": "2023-09-06T02:46",
+        "dateModified": "2023-09-07T03:00",
         "isFamilyFriendly": true,
         "interactivityType": "active",
         "accessibilityControl": [
