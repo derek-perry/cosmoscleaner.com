16e4709840e89ec4a23da1de08a90c3c49adfbdd
DP <dp@derek-perry.com>
2023-12-28 17:13:43 -0500

    Release v0.0.3.4w
    Released v0.0.3.4w on the play page and added changes to the changelog page.

--- pages/changelog.tsx
+++ pages/changelog.tsx
@@ -43,6 +43,12 @@ const changelogPage: NextPage = () => {
                   <li>Adjusted weight display on HUD to disappear after 5 seconds</li>
                   <li>Added collected cargo to cargo bay</li>
                 </ul>
+              <h3 id="0034"><LinkExternal href="https://www.dropbox.com/scl/fi/09j4yxb4zwba25awqltwm/Cosmos-Cleaner-0.0.3.4w.zip?rlkey=3sg3a97zv5g7wzazu74nhb0g4&dl=1" title="Download v0.0.3.4w">v0.0.3.4w</LinkExternal></h3>
+                <p>Released Dec 28, 2023 at 17:00 EST</p>
+                <ul className="text-2xl list-disc pl-8">
+                  <li>Added cargo management in the form of dropping and selling picked up clutter</li>
+                  <li>Fixed modular clutter colors</li>
+                </ul>
           </section>
         </article>
       </main>
